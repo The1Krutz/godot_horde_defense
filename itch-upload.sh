ee351517f4ba04e8128a8abfba9aa1aae7594393
Thomas Kent <the1krutz@gmail.com>
2022-07-16 16:45:55 -0700

    fix build and export scripts

--- itch-upload.sh
+++ itch-upload.sh
@@ -2,6 +2,6 @@
 release="prototype"
 version="0.01"
 
-butler push .export/Linux the1krutz/godot_horde_defense:linux-$release --userversion $version
-butler push .export/Windows the1krutz/godot_horde_defense:windows-$release --userversion $version
-butler push .export/HTML the1krutz/godot_horde_defense:HTML5-$release --userversion $version
+butler push .export/Linux the1krutz/horde-defense:linux-$release --userversion $version
+butler push .export/Windows the1krutz/horde-defense:windows-$release --userversion $version
+butler push .export/HTML the1krutz/horde-defense:HTML5-$release --userversion $version
